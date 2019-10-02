8ea45866b6e651791fd8209a634811acf7fdde82
sleiva <sleiva@PC1_QA.banmedica.corp>
2019-10-02 17:29:01 -0300

    test

--- Scripts/New Test Case/Script1570047868846.groovy
+++ Scripts/New Test Case/Script1570047868846.groovy
@@ -12,4 +12,11 @@ import com.kms.katalon.core.testobject.TestObject as TestObject
 import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+WebUI.openBrowser('')
+
+WebUI.openBrowser('')
+
+WebUI.openBrowser('')
 
